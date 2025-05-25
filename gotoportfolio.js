da3409102e766e5b6e67271593fe79b5141953c5
feliciaspurgeon6367 <feliciaspurgeon6367@gmail.com>
2025-05-25 22:55:55 +0530

    Add files via upload

---
+++ gotoportfolio.js
@@ -0,0 +1,28 @@
+var fetchurl;
+
+    var getdata =  async ()=> {
+        console.log("Func Load")
+        var fetchurl = "";
+        const url = 'https://api.jsonsilo.com/public/846442d1-057f-46dd-ae56-30366a13e402';
+
+        await  fetch(url)
+        .then(response => response.json())  // Parse JSON data
+        .then(data => {
+            // console.log(data.productlist[0].trezior);  // Log the fetched JSON data as an object
+            fetchurl = data.portfolio[2].two;
+            // console.log(fetchurl);
+
+            // return fetchurl;
+
+            window.location.replace(fetchurl);
+
+
+
+            
+
+        })
+        .catch(error => console.error('Error:', error));
+
+    
+
+    };
